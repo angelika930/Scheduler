74c736e8e5a540375c7adb3f70d1f3d3c523754c
migsaenz <saenzm4307@gmail.com>
2025-01-24 14:45:23 -0800

    function prototypes

--- lwp1.c
+++ lwp1.c
@@ -7,8 +7,19 @@
 tid_t lwp_create(lwpfun function, void *arg){
 }
 
-void lwp_start(void){
-}
+//void lwp_start(void){}
 
-void lwp_yield(void){
-}
+//void lwp_yield(void){}
+
+//void lwp_exit(int exitval){}
+
+//tid_t lwp_wait(int *status){}
+
+//tid_t lwp_gettid(void){}
+
+
+//thread tid2thread(tid_t tid){}
+
+//void lwp_set_scheduler(scheduler sched){}
+
+//scheduler lwp_get_scheduler(void){}
